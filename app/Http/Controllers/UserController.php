2c598d90bd7e7c7d3d74146938aec71e5a02949c
BuiKimDat <bkdat@gmail.com>
2024-12-29 17:40:51 +0700

    Wise_social_api

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Helpers\ApiResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class UserController extends Controller
+{
+
+    private $apiResponse;
+
+    public function __construct(){
+        $this->apiResponse = new ApiResponse();
+    }
+
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request)
+    {
+        $userId = Auth::id();
+        if (!$userId) {
+            return $this->apiResponse->UnAuthorization();
+        }
+        $user = User::with('follower', 'follows')
+            ->select(
+                'id', 'name', 'email',
+                'avatar', 'overview'
+            )->where('id', $userId)->first();
+        $user->followers = count($user->follower);
+        $user->following = count($user->follows);
+        unset($user->follower, $user->follows);
+        $folderAvatar = null;
+        if (!is_null($user->avatar)) {
+            $folderAvatar = explode('@', $user->email);
+            $user->avatar = url(
+                'avatars/' . $folderAvatar[0] .'/'. $user->avatar
+            );
+        }
+        return $this->apiResponse->success($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
